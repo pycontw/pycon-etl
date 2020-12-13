96cde2bb4265bb5955be585ad02b586c9c8c28b6
David Chang <davidtnfsh@gmail.com>
2020-12-13 11:45:01 +0800

    feat(dag): add biweekly reporter

--- dags/ods/google_search_console/udfs/google_search.py
+++ dags/ods/google_search_console/udfs/google_search.py
@@ -3,21 +3,13 @@ import heapq
 import requests
 import searchconsole
 
-from dags.config import DISCORD_WEBHOOK
+from config import DISCORD_WEBHOOK
 
 TOPK = 5
 
 
 class GoogleSearchConsoleReporter(object):
     def __init__(self):
-        def get_report():
-            account = searchconsole.authenticate(
-                client_config="client_secret_google_search_console.json",
-                credentials="client_secret_google_search_console_serialized.json",
-            )
-            webproperty = account["https://tw.pycon.org/"]
-            return webproperty.query.range("today", days=-7).dimension("query").get()
-
         self.top_k_ctr = []
         self.top_k_position = []
         self.top_k_clicks = []
@@ -29,10 +21,22 @@ class GoogleSearchConsoleReporter(object):
             self.top_k_impressions,
         ):
             heapq.heapify(top_k_heap)
-        self.report = get_report()
+        self.report = None
 
-    def send_report(self):
-        self.maitain_topk_heap()
+    def main(self):
+        report_msg = self._get_report()
+        self._send_report(report_msg)
+
+    def _get_report(self):
+        account = searchconsole.authenticate(
+            client_config="/usr/local/airflow/dags/client_secret_google_search_console.json",
+            credentials="/usr/local/airflow/dags/client_secret_google_search_console_serialized.json",
+        )
+        webproperty = account["https://tw.pycon.org/"]
+        return webproperty.query.range("today", days=-7).dimension("query").get()
+
+    def _send_report(self, report_msg):
+        self._maitain_topk_heap(report_msg)
         msg_heap_dict = {
             f"透過 google 搜尋點進 PyConTW 官網的所有關鍵字，ctr 最高的前{TOPK}名關鍵字": self.top_k_ctr,
             f"透過 google 搜尋點進 PyConTW 官網的所有關鍵字中，官網排名位置越靠前的前{TOPK}名": self.top_k_position,
@@ -41,23 +45,23 @@ class GoogleSearchConsoleReporter(object):
         }
 
         for msg, heap in msg_heap_dict.items():
-            self.send_msg_to_discord(msg, heap)
-        self.send_team_msg()
+            self._send_msg_to_discord(msg, heap)
+        self._send_team_msg()
 
-    def maitain_topk_heap(self):
+    def _maitain_topk_heap(self, report_msg):
         def heappush(heap, item, topk):
             heapq.heappush(heap, item)
             while len(heap) > topk:
                 heapq.heappop(heap)
 
-        for row in self.report.rows:
+        for row in report_msg.rows:
             heappush(self.top_k_ctr, (row.ctr, row.query), TOPK)
             heappush(self.top_k_position, (-row.position, row.query), TOPK)
             heappush(self.top_k_clicks, (row.clicks, row.query), TOPK)
             heappush(self.top_k_impressions, (row.impressions, row.query), TOPK)
 
     @staticmethod
-    def send_msg_to_discord(msg, heap):
+    def _send_msg_to_discord(msg, heap):
         def get_topk_from_heap(heap):
             def turn_negative_back_to_positive_int(heap):
                 return [(num if num >= 0 else -num, query) for num, query in heap]
@@ -72,19 +76,22 @@ class GoogleSearchConsoleReporter(object):
         requests.post(
             DISCORD_WEBHOOK,
             json={
-                "username": "Data Team 週報",
+                "username": "Data Team 雙週報",
                 "content": f"{msg}：\n {formatted_heap_content}\n----------------------\n",
             },
         )
 
     @staticmethod
-    def send_team_msg():
+    def _send_team_msg():
         requests.post(
             DISCORD_WEBHOOK,
-            json={"username": "Data Team 週報", "content": "有任何問題，歡迎敲 data team 任何一位成員~"},
+            json={
+                "username": "Data Team 雙週報",
+                "content": "有任何問題，歡迎敲 data team 任何一位成員~",
+            },
         )
 
 
 if __name__ == "__main__":
     g = GoogleSearchConsoleReporter()
-    g.send_report()
+    g.main()

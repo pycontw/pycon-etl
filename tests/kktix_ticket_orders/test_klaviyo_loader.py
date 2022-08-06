192f096ccc63e4d32a2102335f76f9ebf0ddddaa
Henry Lee <set>
2022-08-06 18:00:39 +0800

    test: klaviyo loader

--- tests/kktix_ticket_orders/test_klaviyo_loader.py
+++ tests/kktix_ticket_orders/test_klaviyo_loader.py
@@ -1,14 +1,16 @@
 from unittest.mock import patch
 from ods.kktix_ticket_orders.udfs import klaviyo_loader
 
-
-@patch("ods.kktix_ticket_orders.udfs.klaviyo_mailer")
-@patch(
-    "airflow.models.Variable",
-    return_value={
+def fake_airflow_variable():
+    return {
         "KLAVIYO_LIST_ID": "abc",
         "KLAVIYO_CAMPAIGN_ID": "123",
-    },
+    }
+
+@patch("ods.kktix_ticket_orders.udfs.klaviyo_mailer.main")
+@patch(
+    "ods.kktix_ticket_orders.udfs.klaviyo_loader.Variable",
+    new_callable=fake_airflow_variable
 )
 def test_klaviyo_loader(variable, mailer, kktix_api_data):
     klaviyo_loader.load(kktix_api_data)
@@ -16,8 +18,8 @@ def test_klaviyo_loader(variable, mailer, kktix_api_data):
         list_id="abc",
         campaign_id="123",
         campaign_name="隨買即用",
-        datas={
+        datas=[{
             "email": "xxx@gmail.com",
             "name": "李xx",
-        },
+        }]
     )

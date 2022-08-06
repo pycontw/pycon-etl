645980b92274f7297bec58211dfddc8e9aa1e1e1
Henry Lee <set>
2022-08-06 16:35:37 +0800

    fix: mailer data source

--- dags/ods/kktix_ticket_orders/udfs/kktix_loader.py
+++ dags/ods/kktix_ticket_orders/udfs/kktix_loader.py
@@ -3,9 +3,7 @@ import os
 from typing import Dict, List
 
 import pandas as pd
-from airflow.models import Variable
 from google.cloud import bigquery
-from ods.kktix_ticket_orders.udfs import klaviyo_mailer
 from ods.kktix_ticket_orders.udfs.bigquery_loader import TABLE
 
 SCHEMA = [
@@ -29,7 +27,6 @@ def load(event_raw_data_array: List):
         sanitized_event_raw_data = _sanitize_payload(event_raw_data)
         payload.append(sanitized_event_raw_data)
     _load_to_bigquery(payload)
-    _load_to_klaviyo(payload)
 
 
 def _load_to_bigquery(payload: List[Dict]) -> None:
@@ -40,36 +37,6 @@ def _load_to_bigquery(payload: List[Dict]) -> None:
     df = pd.DataFrame(payload, columns=["id", "name", "attendee_info"],)
     job = client.load_table_from_dataframe(df, TABLE, job_config=JOB_CONFIG)
     job.result()
-
-
-def _load_to_klaviyo(payload: List[Dict]) -> None:
-    """
-    Send a notify mail for all participants via third-party service
-    """
-    try:
-        list_id = Variable.get("KLAVIYO_LIST_ID")
-        campaign_id  = Variable.get("KLAVIYO_CAMPAIGN_ID")
-    except KeyError:
-        print("Skip klaviyo mailer, 'KLAVIYO_LIST_ID' or 'KLAVIYO_CAMPAIGN_ID' variable not found")
-        return
-
-    datas = [
-        {
-            "email": item["聯絡人 Email"],
-            "name": item["聯絡人 姓名"],
-        }
-        for item in payload
-    ]
-    if not datas:
-        print("Skip klaviyo mailer, not user profiles")
-        return
-
-    klaviyo_mailer.main(
-        list_id=list_id,
-        campaign_id=campaign_id,
-        campaign_name="隨買即用",
-        datas=datas,
-    )
 
 
 def _sanitize_payload(event_raw_data) -> Dict:

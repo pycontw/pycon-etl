075f37d895ba06de255b7fd84018013bedb855d7
Henry Lee <set>
2023-05-06 21:23:28 +0800

    refactor(ods): create kktix refund dag, move discord bot to app module

---
+++ dags/ods/kktix_ticket_orders/kktix_refund_dag.py
@@ -0,0 +1,34 @@
+"""
+Update KKTIX's data if attendee has been refunded
+"""
+from datetime import datetime, timedelta
+
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+from ods.kktix_ticket_orders.udfs import kktix_refund
+
+DEFAULT_ARGS = {
+    "owner": "henry410213028@gmail.com",
+    "depends_on_past": False,
+    "start_date": datetime(2023, 4, 10, 4),
+    "retries": 2,
+    "retry_delay": timedelta(minutes=5),
+    "on_failure_callback": lambda x: "Need to send notification to Discord!",
+}
+dag = DAG(
+    "KKTIX_TICKET_REFUND_V2",
+    default_args=DEFAULT_ARGS,
+    schedule_interval="50 23 * * *",  # At 23:50 (everyday)
+    max_active_runs=1,
+    catchup=True,
+)
+with dag:
+    UPDATE_REFUNDED_ATTENDEE_IDS = PythonOperator(
+        task_id="UPDATE_REFUNDED_ATTENDEE_IDS",
+        python_callable=kktix_refund.main,
+    )
+
+    UPDATE_REFUNDED_ATTENDEE_IDS
+
+if __name__ == "__main__":
+    dag.cli()
--- dags/ods/kktix_ticket_orders/udfs/batch_kktix2mailer.py
+++ dags/ods/kktix_ticket_orders/udfs/batch_kktix2mailer.py
@@ -1,20 +1,5 @@
-from typing import Callable, Dict, List
-
-import requests
-import tenacity
-from airflow.hooks.http_hook import HttpHook
-from airflow.models import Variable
 from dateutil.parser import parse
-from ods.kktix_ticket_orders.udfs import klaviyo_loader
-
-SCHEDULE_INTERVAL_SECONDS: int = 60 * 60
-HTTP_HOOK = HttpHook(http_conn_id="kktix_api", method="GET")
-RETRY_ARGS = dict(
-    wait=tenacity.wait_none(),
-    stop=tenacity.stop_after_attempt(3),
-    retry=tenacity.retry_if_exception_type(requests.exceptions.ConnectionError),
-)
-
+from ods.kktix_ticket_orders.udfs import kktix_api, klaviyo_loader
 
 def main(**context):
     """
@@ -26,105 +11,7 @@ def main(**context):
     ts_datetime_obj = parse(context["ts"])
     year = ts_datetime_obj.year
     timestamp = ts_datetime_obj.timestamp()
-    event_raw_data_array = _extract(year=year, timestamp=timestamp,)
+    event_raw_data_array = kktix_api._extract(year=year, timestamp=timestamp,)
     # load name and email to mailer before data has been hashed
     klaviyo_loader.load(event_raw_data_array)
     print(f"Batch load {len(event_raw_data_array)} data to downstream task")
-
-
-def _extract(year: int, timestamp: float) -> List[Dict]:
-    """
-    get data from KKTIX's API
-    1. condition_filter_callb: use this callbacl to filter out unwanted event!
-    2. right now schedule_interval_seconds is a hardcoded value!
-    """
-    event_raw_data_array: List[Dict] = []
-    condition_filter_callback = (
-        lambda event: str(year) in event["name"]
-        and "registration" in event["name"].lower()
-    )
-    event_metadatas = get_event_metadatas(condition_filter_callback)
-    for event_metadata in event_metadatas:
-        event_id = event_metadata["id"]
-        for attendee_info in get_attendee_infos(event_id, timestamp):
-            event_raw_data_array.append(
-                {
-                    "id": event_id,
-                    "name": event_metadata["name"],
-                    "attendee_info": attendee_info,
-                }
-            )
-    return event_raw_data_array
-
-
-def get_attendee_infos(event_id: int, timestamp: float) -> List:
-    """
-    it's a public wrapper for people to get attendee infos!
-    """
-    attendance_book_id = _get_attendance_book_id(event_id)
-    attendee_ids = _get_attendee_ids(event_id, attendance_book_id)
-    attendee_infos = _get_attendee_infos(event_id, attendee_ids, timestamp)
-    return attendee_infos
-
-
-def get_event_metadatas(condition_filter: Callable) -> List[Dict]:
-    """
-    Fetch all the ongoing events
-    """
-    event_list_resp = HTTP_HOOK.run_with_advanced_retry(
-        endpoint=f"{Variable.get('kktix_events_endpoint')}?only_not_ended_event=true",
-        _retry_args=RETRY_ARGS,
-    ).json()
-    event_metadatas: List[dict] = []
-    for event in event_list_resp["data"]:
-        if condition_filter(event):
-            event_metadatas.append(event)
-    return event_metadatas
-
-
-def _get_attendance_book_id(event_id: int) -> int:
-    """
-    Fetch attendance books
-    """
-    attendance_books_resp = HTTP_HOOK.run_with_advanced_retry(
-        endpoint=f"{Variable.get('kktix_events_endpoint')}/{event_id}/attendance_books",
-        _retry_args=RETRY_ARGS,
-    ).json()
-    return attendance_books_resp[0]["id"]
-
-
-def _get_attendee_ids(event_id: int, attendance_book_id: int) -> List[int]:
-    """
-    get all attendee ids!
-    """
-    attendee_ids = []
-    attendees_resp = HTTP_HOOK.run_with_advanced_retry(
-        endpoint=f"{Variable.get('kktix_events_endpoint')}/{event_id}/attendance_books/{attendance_book_id}",
-        _retry_args=RETRY_ARGS,
-    ).json()
-    for signin_status_tuple in attendees_resp["signin_status"]:
-        attendee_ids.append(signin_status_tuple[0])
-    return attendee_ids
-
-
-def _get_attendee_infos(
-    event_id: int, attendee_ids: List[int], timestamp: float
-) -> List:
-    """
-    get attendee infos, e.g. email, phonenumber, name and etc
-    """
-    attendee_infos = []
-    for attendee_id in attendee_ids:
-        attendee_info = HTTP_HOOK.run_with_advanced_retry(
-            endpoint=f"{Variable.get('kktix_events_endpoint')}/{event_id}/attendees/{attendee_id}",
-            _retry_args=RETRY_ARGS,
-        ).json()
-        if not attendee_info["is_paid"]:
-            continue
-        if (
-            timestamp
-            < attendee_info["updated_at"]
-            < timestamp + SCHEDULE_INTERVAL_SECONDS
-        ):
-            attendee_infos.append(attendee_info)
-    return attendee_infos

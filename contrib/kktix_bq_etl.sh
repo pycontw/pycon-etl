81317032c6f04c7599d22be920b86b85870b2729
Angus Yang <googolangus@gmail.com>
2023-07-02 14:06:29 +0800

    [refactor] (dataframe): Clean the columns for 2022

--- contrib/kktix_bq_etl.sh
+++ contrib/kktix_bq_etl.sh
@@ -8,7 +8,7 @@ cmd=${PWD}/kktix_bq_etl.py
 
 for ticket_type in corporate individual reserved
 do
-    suffix=${ticket_type}_attendees_test
+    suffix=${ticket_type}_attendees$2
     cmd_args="-p ${project_id} -d dwd -t kktix_ticket_${suffix} -k ${ticket_type} -y $1 --upload"
     echo ${cmd_args}
     ${cmd} ${cmd_args}

61cb7258332e0935e07e21a532fc79e13eb9bf2f
Henry Lee <set>
2024-05-25 18:02:40 +0800

    fix(ods): fill alert email

--- dags/airlfow-db-cleanup.py
+++ dags/airlfow-db-cleanup.py
@@ -56,7 +56,7 @@ SCHEDULE_INTERVAL = "@daily"
 # Who is listed as the owner of this DAG in the Airflow Web Server
 DAG_OWNER_NAME = "operations"
 # List of email address to send email alerts to if this job fails
-ALERT_EMAIL_ADDRESSES = []
+ALERT_EMAIL_ADDRESSES = ["henry410213028@gmail.com"]
 # Length to retain the log files if not already provided in the conf. If this
 # is set to 30, the job will remove those files that arE 30 days old or older.
 

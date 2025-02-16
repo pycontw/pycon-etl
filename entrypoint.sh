b3badb1a889db892d1725ccd21957d2d0bb49b6c
Wei Lee <weilee.rx@gmail.com>
2025-02-16 16:33:52 +0800

    build(docker-compose): use "airflow db init" instead of deprecated "airflow initdb"

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,38 +4,38 @@ set -e
 
 # Check if the AIRFLOW_HOME variable is set
 if [ -z "${AIRFLOW_HOME}" ]; then
-  echo 'AIRFLOW_HOME not set'
-  exit 1
+    echo 'AIRFLOW_HOME not set'
+    exit 1
 fi
 
 # Create Fernet key if not exists
 if [ -z "${AIRFLOW__CORE__FERNET_KEY}" ]; then
-  echo "Fernet key not set. Generating a new one."
-  export AIRFLOW__CORE__FERNET_KEY=$(python -c 'from cryptography.fernet import Fernet; print(Fernet.generate_key().decode())')
-  echo "Fernet key generated and set."
-  echo "[WARNING] Please save the AIRFLOW__CORE__FERNET_KEY for future use."
+    echo "Fernet key not set. Generating a new one."
+    export AIRFLOW__CORE__FERNET_KEY=$(python -c 'from cryptography.fernet import Fernet; print(Fernet.generate_key().decode())')
+    echo "Fernet key generated and set."
+    echo "[WARNING] Please save the AIRFLOW__CORE__FERNET_KEY for future use."
 else
-  echo "Fernet key exists."
+    echo "Fernet key exists."
 fi
 
 # Check if the database exists and initialize it if not
 if [ ! -f "${AIRFLOW_HOME}/airflow.db" ]; then
-  airflow initdb
-  echo 'Database initialized'
+    airflow db init
+    echo 'Database initialized'
 else
-  echo 'Database existed'
+    echo 'Database existed'
 fi
 
 # Check if the GCP service account is provided
 if [ -z "${GOOGLE_APPLICATION_CREDENTIALS}" ]; then
-  echo "No GCP service account provided, set to default path"
-  export GOOGLE_APPLICATION_CREDENTIALS="${AIRFLOW_HOME}/service-account.json"
+    echo "No GCP service account provided, set to default path"
+    export GOOGLE_APPLICATION_CREDENTIALS="${AIRFLOW_HOME}/service-account.json"
 fi
 
 # Check if the command is provided
 if [ -z "$1" ]; then
-  echo "No command provided. Usage: $0 {airflow_command}"
-  exit 1
+    echo "No command provided. Usage: $0 {airflow_command}"
+    exit 1
 fi
 
 # Execute the provided Airflow command

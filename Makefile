7a823253141b347541bfa1ee622ee7cbadbad75b
Xch1 <xch1@Xch1-Macbook-Air.local>
2025-08-22 09:08:02 +0800

    add user_profile gemini

--- Makefile
+++ Makefile
@@ -14,13 +14,13 @@ coverage:
 	PYTHONPATH=./dags uv run pytest --cov=dags tests
 
 build-dev:
-	docker-compose -f ./docker-compose-dev.yml build
+	docker compose -f ./docker-compose-dev.yml build
 
 deploy-dev:
-	docker-compose -f ./docker-compose-dev.yml up -d
+	docker compose -f ./docker-compose-dev.yml up -d
 
 down-dev:
-	docker-compose -f ./docker-compose-dev.yml down
+	docker compose -f ./docker-compose-dev.yml down
 
 deploy-prod:
 	docker-compose -f ./docker-compose.yml up -d
---
+++ dags/app/user_profile/dag.py
@@ -0,0 +1,41 @@
+"""
+Query user profile catagory
+"""
+from datetime import datetime, timedelta
+
+from airflow.sdk import Variable, dag, task
+
+from dags.app.user_profile.udf import (
+    process_table
+)
+
+DEFAULT_ARGS = {
+    "owner": "Xch1",
+    "depends_on_past": False,
+    "start_date": datetime(2025, 8, 20),
+    "retries": 2,
+    "retry_delay": timedelta(minutes=5),
+    "on_failure_callback": lambda x: "need to query updates ",
+}
+
+
+@dag(
+    default_args=DEFAULT_ARGS,
+    schedule=None,
+    max_active_runs=1,
+    catchup=False,
+)
+def USER_PROFILE_CATAGORY_QUERY():
+    @task
+    def PROCESS_CATAGORY_BY_GEMINI():
+        tasktypes = ["organization", "job_title"]
+        for tasktype in tasktypes:
+            process_table("gemini-2.0-flash", 8192, 100, tasktype)
+
+    PROCESS_CATAGORY_BY_GEMINI()
+
+
+dag_obj = USER_PROFILE_CATAGORY_QUERY()
+
+if __name__ == "__main__":
+    dag_obj.test()
